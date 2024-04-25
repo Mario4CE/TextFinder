67124b18b3186197af477f4c3ee726fef4fe652f
Mario4CE <m.cerdas@estudiantec.cr>
2024-04-24 23:05:06 -0600

    They are added to search in .txt and .docx

--- TextFinder/src/main/java/module-info.java
+++ TextFinder/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.textfinder {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.apache.pdfbox;
+    requires org.apache.poi.ooxml;
 
 
     opens org.example.textfinder to javafx.fxml;
